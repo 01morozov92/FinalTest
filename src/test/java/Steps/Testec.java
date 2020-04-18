c27ac4846fabc97b18121a3124ccc8b44f53758f
01morozov92 <01morozov92@gmail.com>
2020-04-18 14:23:36 +0300

    adding maximized window

--- src/test/java/Steps/Testec.java
+++ src/test/java/Steps/Testec.java
@@ -1,13 +1,17 @@
 package Steps;
 
+import com.codeborne.selenide.Configuration;
 import io.cucumber.java.Before;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class Testec {
-    @Before
+    @Before()
+
     public void tesTec () {
+        Configuration.startMaximized = true;
         open("https://cucumber.netlify.app/");
     }
 }
